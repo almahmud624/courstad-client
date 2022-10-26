6d374af1500ed546fbb4f585d68ce3666d7c784a
almahmud624 <mahmudbhuiyan624@gmail.com>
2022-10-26 14:09:54 +0600

    login and register page resdsign, auth error message added

--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -1,22 +1,22 @@
 import React from "react";
 import { useContext } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { AuthContext } from "../../Contexts/AuthProvider";
+import { FaGithub, FaGoogle, FaLink, FaUserAlt } from "react-icons/fa";
+import { GrMail } from "react-icons/gr";
+import { RiLockPasswordFill } from "react-icons/ri";
+import { useState } from "react";
 
 const Register = () => {
-  const { userGoogleSignIn, createUser, updateUserProfie } =
+  const { userGoogleSignIn, createUser, updateUserProfie, userGitHubSignIn } =
     useContext(AuthContext);
-  const location = useLocation();
-  const navigate = useNavigate();
-  const from = location?.state?.from?.pathname || "/";
+  const [err, setErr] = useState("");
   // google signin
   const handleGoogleSignIn = () => {
     userGoogleSignIn()
-      .then((res) => {
-        navigate(from, { replace: true });
-      })
+      .then((res) => {})
       .catch((error) => {
-        console.log(error.code);
+        setErr(error.code);
       });
   };
   // create user
@@ -26,7 +26,7 @@ const Register = () => {
     const name = form.name.value;
     const email = form.email.value;
     const password = form.password.value;
-    console.log(password);
+    setErr("");
 
     createUser(email, password)
       .then((res) => {
@@ -34,95 +34,114 @@ const Register = () => {
         console.log(res.user);
       })
       .catch((error) => {
-        console.log(error.code);
+        setErr(error.code);
       });
   };
+
+  // update user profile
   const handleUserProfileUpdate = (name) => {
     updateUserProfie({ displayName: name })
       .then(() => {})
-      .catch((error) => {});
+      .catch((error) => {
+        setErr(error.code);
+      });
+  };
+
+  // login with gitHub
+  const handleGitHubSignIn = () => {
+    userGitHubSignIn()
+      .then((res) => {})
+      .catch((error) => {
+        setErr(error.code);
+      });
   };
   return (
     <div>
       <section>
-        <div class="flex min- overflow-hidden">
-          <div class="flex flex-col justify-center flex-1 px-4 py-12 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
+        <div class="flex overflow-hidden justify-center items-center">
+          <div class="flex flex-col justify-center items-center flex-1 px-4 py-12 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
             <div class="w-full max-w-xl mx-auto lg:w-96">
               <div>
-                <a href="./index.html" class="text-blue-600 text-medium">
-                  wickedblocks
-                </a>
-                <h2 class="mt-6 text-3xl font-extrabold text-neutral-600">
-                  Sign in.
+                <h2 class="mt-6 text-3xl font-bold text-neutral-600">
+                  Create an Account
                 </h2>
               </div>
-
+              {err && (
+                <div
+                  role="alert"
+                  class="rounded border-l-4 border-red-500 bg-red-50 p-4 mt-8"
+                >
+                  <strong class="block capitalize font-medium text-red-700">
+                    {" "}
+                    {err.split("/")[1].split("-").join(" ")}{" "}
+                  </strong>
+                </div>
+              )}
               <div class="mt-8">
                 <div class="mt-6">
                   <form onSubmit={handleSubmit} method="POST" class="space-y-6">
-                    <div>
-                      <label
-                        for="name"
-                        class="block text-sm font-medium text-neutral-600"
-                      >
-                        {" "}
-                        Your Name{" "}
-                      </label>
-                      <div class="mt-1">
+                    <div class="flex flex-col mb-2">
+                      <div class="flex relative ">
                         <input
+                          type="text"
                           id="name"
                           name="name"
-                          type="text"
-                          autocomplete="name"
-                          required=""
+                          class=" rounded-l-md flex-1 appearance-none  w-full py-2 px-4  text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:border-transparent focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-300 bg-white border-t border-l border-b  border-gray-300 transition duration-500 ease-in-out transform"
                           placeholder="Your Name"
-                          class="block w-full px-5 py-3 text-base placeholder-gray-300 transition duration-500 ease-in-out transform border border-transparent rounded-lg text-neutral-600 bg-gray-50 focus:outline-none focus:border-transparent focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-300"
+                          required
                         />
+                        <span class="rounded-r-lg inline-flex  items-center px-3 border-t bg-white border border-gray-300 text-gray-500 shadow-sm text-sm">
+                          <FaUserAlt />
+                        </span>
                       </div>
                     </div>
-                    <div>
-                      <label
-                        for="email"
-                        class="block text-sm font-medium text-neutral-600"
-                      >
-                        {" "}
-                        Email address{" "}
-                      </label>
-                      <div class="mt-1">
+                    <div class="flex flex-col mb-2">
+                      <div class="flex relative ">
                         <input
+                          type="text"
+                          id="picUrl"
+                          name="picUrl"
+                          class=" rounded-l-md flex-1 appearance-none  w-full py-2 px-4  text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:border-transparent focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-300 bg-white border-t border-l border-b  border-gray-300 transition duration-500 ease-in-out transform"
+                          placeholder="Profile Picture URL"
+                          required
+                        />
+                        <span class="rounded-r-lg inline-flex  items-center px-3 border-t bg-white border border-gray-300 text-gray-500 shadow-sm text-sm">
+                          <FaLink />
+                        </span>
+                      </div>
+                    </div>
+                    <div class="flex flex-col mb-2">
+                      <div class="flex relative ">
+                        <input
+                          type="email"
                           id="email"
                           name="email"
-                          type="email"
-                          autocomplete="email"
-                          required=""
-                          placeholder="Your Email"
-                          class="block w-full px-5 py-3 text-base placeholder-gray-300 transition duration-500 ease-in-out transform border border-transparent rounded-lg text-neutral-600 bg-gray-50 focus:outline-none focus:border-transparent focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-300"
+                          class=" rounded-l-md flex-1 appearance-none  w-full py-2 px-4  text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:border-transparent focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-300 bg-white border-t border-l border-b  border-gray-300 transition duration-500 ease-in-out transform"
+                          placeholder="Your email"
+                          required
                         />
+                        <span class="rounded-r-lg inline-flex  items-center px-3 border-t bg-white border border-gray-300 text-gray-500 shadow-sm text-sm">
+                          <GrMail />
+                        </span>
                       </div>
                     </div>
-
-                    <div class="space-y-1">
-                      <label
-                        for="password"
-                        class="block text-sm font-medium text-neutral-600"
-                      >
-                        {" "}
-                        Password{" "}
-                      </label>
-                      <div class="mt-1">
+                    <div class="flex flex-col mb-2">
+                      <div class="flex relative ">
                         <input
+                          type="password"
                           id="password"
                           name="password"
-                          type="password"
-                          autocomplete="current-password"
-                          required=""
+                          class=" rounded-l-md flex-1 appearance-none  w-full py-2 px-4  text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:border-transparent focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-300 bg-white border-t border-l border-b  border-gray-300 transition duration-500 ease-in-out transform"
                           placeholder="Your Password"
-                          class="block w-full px-5 py-3 text-base placeholder-gray-300 transition duration-500 ease-in-out transform border border-transparent rounded-lg text-neutral-600 bg-gray-50 focus:outline-none focus:border-transparent focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-300"
+                          required
                         />
+                        <span class="rounded-r-lg inline-flex  items-center px-3 border-t bg-white border border-gray-300 text-gray-500 shadow-sm text-sm">
+                          <RiLockPasswordFill />
+                        </span>
                       </div>
                     </div>
 
-                    <div class="flex items-center justify-between">
+                    {/* <div class="flex items-center justify-between">
                       <div class="flex items-center">
                         <input
                           id="remember-me"
@@ -149,14 +168,14 @@ const Register = () => {
                           Forgot your password?{" "}
                         </a>
                       </div>
-                    </div>
+                    </div> */}
 
                     <div>
                       <button
                         type="submit"
-                        class="flex items-center justify-center w-full px-10 py-4 text-base font-medium text-center text-white transition duration-500 ease-in-out transform bg-blue-600 rounded-xl hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+                        class="flex items-center justify-center w-full px-10 py-3 text-base font-medium text-center text-white transition duration-500 ease-in-out transform bg-blue-600 rounded hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
                       >
-                        Sign in
+                        Create an Account
                       </button>
                     </div>
                   </form>
@@ -171,59 +190,34 @@ const Register = () => {
                       </span>
                     </div>
                   </div>
-                  <div>
+                  <div className="flex justify-center gap-8">
                     <button
-                      onClick={handleGoogleSignIn}
-                      type="submit"
-                      class="w-full items-center block px-10 py-3.5 text-base font-medium text-center text-blue-600 transition duration-500 ease-in-out transform border-2 border-white shadow-md rounded-xl focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+                      class="inline-flex items-center justify-center rounded-md border-2 border-[#171515] bg-[#171515]  py-3 text-sm font-medium text-white transition-colors hover:bg-transparent hover:text-[#171515] focus:outline-none focus:ring active:opacity-75 w-full"
+                      onClick={handleGitHubSignIn}
                     >
-                      <div class="flex items-center justify-center">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          class="w-6 h-6"
-                          viewBox="0 0 48 48"
-                        >
-                          <defs>
-                            <path
-                              id="a"
-                              d="M44.5 20H24v8.5h11.8C34.7 33.9 30.1 37 24 37c-7.2 0-13-5.8-13-13s5.8-13 13-13c3.1 0 5.9 1.1 8.1 2.9l6.4-6.4C34.6 4.1 29.6 2 24 2 11.8 2 2 11.8 2 24s9.8 22 22 22c11 0 21-8 21-22 0-1.3-.2-2.7-.5-4z"
-                            ></path>
-                          </defs>
-                          <path
-                            clip-path="url(#b)"
-                            fill="#FBBC05"
-                            d="M0 37V11l17 13z"
-                          ></path>
-                          <path
-                            clip-path="url(#b)"
-                            fill="#EA4335"
-                            d="M0 11l17 13 7-6.1L48 14V0H0z"
-                          ></path>
-                          <path
-                            clip-path="url(#b)"
-                            fill="#34A853"
-                            d="M0 37l30-23 7.9 1L48 0v48H0z"
-                          ></path>
-                          <path
-                            clip-path="url(#b)"
-                            fill="#4285F4"
-                            d="M48 48L17 24l-4-3 35-10z"
-                          ></path>
-                        </svg>
-                        <span class="ml-4"> Log in with Google</span>
-                      </div>
+                      GitHub <FaGithub className="ml-2" />
                     </button>
+                    <button
+                      class="inline-flex items-center justify-center rounded-md border-2 border-green-700 bg-green-700 py-3 text-sm font-medium text-white transition-colors hover:bg-transparent hover:text-green-700 focus:outline-none focus:ring active:opacity-75 w-full"
+                      onClick={handleGoogleSignIn}
+                    >
+                      Google <FaGoogle className="ml-2" />
+                    </button>
+                  </div>
+                  <div class="flex justify-center items-center mt-3 p-4">
+                    <p class="text-gray-500 text-sm text-center">
+                      Already Courstad user?{" "}
+                      <Link
+                        to="/login"
+                        class="text-indigo-500 hover:text-indigo-600 active:text-indigo-700 transition duration-100"
+                      >
+                        Login
+                      </Link>
+                    </p>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div class="relative flex-1 hidden w-0 overflow-hidden lg:block">
-            <img
-              class="absolute inset-0 object-cover w-full h-full"
-              src="/assets/images/placeholders/rectangleWide.png"
-              alt=""
-            />
           </div>
         </div>
       </section>
